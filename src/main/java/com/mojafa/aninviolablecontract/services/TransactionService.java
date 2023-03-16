b070c1478410b161005d3b2a19c4e42178136016
MoJafa <jmohsa@gmail.com>
2023-03-16 23:22:10 +0200

    created transaction service

---
+++ src/main/java/com/mojafa/aninviolablecontract/services/TransactionService.java
@@ -0,0 +1,14 @@
+package com.mojafa.aninviolablecontract.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+    //dd a method findAllByAccountNumber,
+    // which should return a list of transactions.
+    // For now, have it return an empty list.
+    public void findAllByAccountNumber() {
+    }
+
+
+}
---
+++ src/test/java/com/mojafa/aninviolablecontract/TransactionServiceTest.java
@@ -0,0 +1,2 @@
+package com.mojafa.aninviolablecontract;public class TransactionServiceTest {
+}
