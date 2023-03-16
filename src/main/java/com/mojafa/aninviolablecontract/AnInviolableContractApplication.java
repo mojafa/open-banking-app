fcdbb57b1f7af8a8a7bdd6e4540c69dd707eb8e3
MoJafa <jmohsa@gmail.com>
2023-03-16 23:04:21 +0200

    initiated project with proper file structure

---
+++ src/main/java/com/mojafa/aninviolablecontract/AnInviolableContractApplication.java
@@ -0,0 +1,13 @@
+package com.mojafa.aninviolablecontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnInviolableContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnInviolableContractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mojafa/aninviolablecontract/AnInviolableContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojafa.aninviolablecontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnInviolableContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
