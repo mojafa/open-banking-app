ff8379f48e6775aed29e613e72ce797ba79d36ef
MoJafa <jmohsa@gmail.com>
2023-03-17 23:18:15 +0200

    added component test

--- src/test/java/com/mojafa/aninviolablecontract/TransactionComponentTest.java
+++ src/test/java/com/mojafa/aninviolablecontract/TransactionComponentTest.java
@@ -1,2 +1,16 @@
-package com.mojafa.aninviolablecontract;public class TransactionComponentTest {
+package com.mojafa.aninviolablecontract;
+
+public class TransactionComponentTest {
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testApplicationEndToEnd() {
+        given().standaloneSetup(new TransactionController(new TransactionService()))
+                .when()
+                .get(String.format("http://localhost:%s/api/v1/transactions/1234567", port))
+                .then()
+                .statusCode(Matchers.is(200));
+    }
+
 }
