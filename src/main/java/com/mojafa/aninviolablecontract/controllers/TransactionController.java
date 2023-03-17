76f52b66433ac34e3f6817b9d6a4a345cf0398da
MoJafa <jmohsa@gmail.com>
2023-03-17 23:35:52 +0200

    minor edits

--- src/main/java/com/mojafa/aninviolablecontract/controllers/TransactionController.java
+++ src/main/java/com/mojafa/aninviolablecontract/controllers/TransactionController.java
@@ -2,14 +2,15 @@ package com.mojafa.aninviolablecontract.controllers;
 
 
 import com.mojafa.aninviolablecontract.dto.TransactionDTO;
+import com.mojafa.aninviolablecontract.models.Transaction;
 import com.mojafa.aninviolablecontract.services.TransactionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
@@ -21,7 +22,7 @@ public class TransactionController {
     }
 
     @GetMapping("/{accountNumber}")
-    public List<Transaction> getTransactions(@PathVariable long accountNumber) {
+    public List<TransactionDTO> getTransactions(@PathVariable long accountNumber) {
         List<Transaction> transactions = transactionService.findAllByAccountNumber(accountNumber);
         List<TransactionDTO> transactionDTOs = new ArrayList<>();
         for (Transaction transaction : transactions) {
