efe8a92be778a12ee7df18e51a6d3b14b565a6a0
MoJafa <jmohsa@gmail.com>
2023-03-17 23:17:52 +0200

    added controller test

--- src/test/java/com/mojafa/aninviolablecontract/controllers/TransactionControllerTest.java
+++ src/test/java/com/mojafa/aninviolablecontract/controllers/TransactionControllerTest.java
@@ -1,2 +1,27 @@
-package com.mojafa.aninviolablecontract.controllers;public class TransactionControllerTest {
+package com.mojafa.aninviolablecontract.controllers;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class TransactionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        mockMvc.perform(
+                get("/api/v1/transactions/1"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
 }
