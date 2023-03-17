e159ee4ae9ab2731105020657b640a58701f3c2b
MoJafa <jmohsa@gmail.com>
2023-03-18 00:03:27 +0200

    modified service

--- src/main/java/com/mojafa/aninviolablecontract/services/TransactionService.java
+++ src/main/java/com/mojafa/aninviolablecontract/services/TransactionService.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 @Service
 public class TransactionService {
-    private final TransactionRepository transactionRepository;
+    private TransactionRepository transactionRepository;
     public TransactionService(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
+        this.transactionRepository = this.transactionRepository;
     }
     public List<Transaction> findAllByAccountNumber(long accountNumber) {
         return transactionRepository.findAllByAccountNumber(accountNumber);
