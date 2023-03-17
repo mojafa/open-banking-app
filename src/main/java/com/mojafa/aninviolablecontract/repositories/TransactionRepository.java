27e1c0120c4db7de8c7987c102dd1ebce304407f
MoJafa <jmohsa@gmail.com>
2023-03-17 22:36:01 +0200

    edited repo class

--- src/main/java/com/mojafa/aninviolablecontract/repositories/TransactionRepository.java
+++ src/main/java/com/mojafa/aninviolablecontract/repositories/TransactionRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findAllByAccountNumber(int accountNumber);
+    List<Transaction> findAllByAccountNumber( Integer accountNumber);
 }
