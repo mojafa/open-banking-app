3450e74b29cd9e05e6502e6d53144c6559e3e01d
MoJafa <jmohsa@gmail.com>
2023-03-17 23:18:26 +0200

    edited service

--- src/main/java/com/mojafa/aninviolablecontract/services/TransactionService.java
+++ src/main/java/com/mojafa/aninviolablecontract/services/TransactionService.java
@@ -1,25 +1,21 @@
 package com.mojafa.aninviolablecontract.services;
 
-import com.mojafa.aninviolablecontract.models.Transaction;
+import com.mojafa.aninviolablecontract.dto.TransactionDTO;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
-        public List<Transaction> findAllByAccountNumber(String accountNumber) {
-            // For now, just return an empty list
-//            return new ArrayList<>();
-            return Arrays.asList(
-                    new Transaction("debit", new Date(), accountNumber, "USD", BigDecimal.valueOf(100.00), "Merchant 1", "Merchant Logo 1"),
-                    new Transaction("credit", new Date(), accountNumber, "USD", BigDecimal.valueOf(200.00), "Merchant 2", "Merchant Logo 2"),
-                    new Transaction("debit", new Date(), accountNumber, "USD", BigDecimal.valueOf(300.00), "Merchant 3", "Merchant Logo 3")
-            );
-        }
+    private final TransactionRepository transactionRepository;
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
     }
+    public List<Transaction> findAllByAccountNumber(long accountNumber) {
+        return transactionRepository.getTransactionsByAccountNumber(accountNumber);
+    }
+}
 
 
 
