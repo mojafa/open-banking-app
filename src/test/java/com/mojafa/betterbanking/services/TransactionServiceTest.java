953a2642494b575d46826a01eabab36211ee40eb
MoJafa <jmohsa@gmail.com>
2023-03-30 23:00:29 +0200

    fixed minor bug on service test

--- src/test/java/com/mojafa/betterbanking/services/TransactionServiceTest.java
+++ src/test/java/com/mojafa/betterbanking/services/TransactionServiceTest.java
@@ -1,13 +1,16 @@
 package com.mojafa.betterbanking.services;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-
+@SpringBootTest
 class TransactionServiceTest {
-    TransactionService transactionService = new TransactionService();
+    @Autowired
+    private TransactionService transactionService;
     @Test
     public void testTransactionCount() {
         assertEquals(1, transactionService.findAllByAccountNumber(1234567).size());
