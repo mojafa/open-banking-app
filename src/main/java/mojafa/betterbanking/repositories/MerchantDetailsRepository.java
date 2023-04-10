64280b3f5e16238a158408825d5f3322a1039c05
MoJafa <jmohsa@gmail.com>
2023-04-10 11:29:41 +0200

    added optional of merchant logos

--- src/main/java/mojafa/betterbanking/repositories/MerchantDetailsRepository.java
+++ src/main/java/mojafa/betterbanking/repositories/MerchantDetailsRepository.java
@@ -1,4 +1,7 @@
 package mojafa.betterbanking.repositories;
 
+import java.util.Optional;
+
 public interface MerchantDetailsRepository {
+    Optional<String> findMerchantLogo(final String merchantName);
 }
