0e4dafe5f17caf7197aca53d832cc53698529929
MoJafa <jmohsa@gmail.com>
2023-04-10 12:20:56 +0200

    added webclient

--- src/main/java/mojafa/betterbanking/BetterBankingApplication.java
+++ src/main/java/mojafa/betterbanking/BetterBankingApplication.java
@@ -1,11 +1,23 @@
-package com.mojafa.betterbanking;
+package mojafa.betterbanking;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BetterBankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BetterBankingApplication.class, args);
 	}
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder()
+				.baseUrl(baseUrl)
+				.build();
+	}
+
+	@Value( "" )
+	private String baseUrl;
 }
