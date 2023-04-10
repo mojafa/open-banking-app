b9c522f354739177bb3d336dd7698a329026b302
MoJafa <jmohsa@gmail.com>
2023-04-10 12:55:05 +0200

    added ApplicationRunner to construct some Transactions and store them in our MySQL database

--- src/main/java/mojafa/betterbanking/BetterBankingApplication.java
+++ src/main/java/mojafa/betterbanking/BetterBankingApplication.java
@@ -1,10 +1,16 @@
 package mojafa.betterbanking;
 
+import mojafa.betterbanking.models.Transaction;
+import mojafa.betterbanking.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BetterBankingApplication {
@@ -16,6 +22,27 @@ public class BetterBankingApplication {
 		return WebClient.builder()
 				.baseUrl(baseUrl)
 				.build();
+	}
+
+
+
+	@Profile(value = "dev")
+	@Bean
+	public ApplicationRunner runner(final TransactionRepository repository) {
+		return args -> {
+			repository.deleteAll();
+			var t = Transaction
+					.builder()
+					.type("Credit")
+					.date(new Date())
+					.accountNumber(1234567)
+					.currency("USD")
+					.amount(100.00)
+					.merchantName("Globex")
+					.merchantLogo("images/globex.png")
+					.build();
+			repository.save(t);
+		};
 	}
 
 	@Value( "" )
