06fddb59dd13ff913d22bbc3422223095713eace
MoJafa <jmohsa@gmail.com>
2023-04-10 11:02:55 +0200

    added webflux and resilience4j dependencies

--- src/main/java/mojafa/betterbanking/repositories/RESTTransactionsAPIClient.java
+++ src/main/java/mojafa/betterbanking/repositories/RESTTransactionsAPIClient.java
@@ -1,4 +1,12 @@
 package mojafa.betterbanking.repositories;
 
-public class RESTTransactionsAPIClient {
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RESTTransactionsAPIClient implements TransactionApiClient
+@Autowired
+public RESTTransactionsAPIClient(final WebClient webClient) {
+    this.webClient = webClient;
+}
+        private final WebClient client;
+
 }
