2bef9e34ffb995ba11188e7ed36906dab04365c1
MoJafa <jmohsa@gmail.com>
2023-04-10 10:57:06 +0200

    defined transactionapiclient interface

--- src/main/java/mojafa/betterbanking/repositories/TransactionApiClient.java
+++ src/main/java/mojafa/betterbanking/repositories/TransactionApiClient.java
@@ -1,7 +1,9 @@
 package mojafa.betterbanking.repositories;
 
+import mojafa.betterbanking.models.Transaction;
+
 import java.util.List;
 
 public interface TransactionApiClient {
-    List<com.mojafa.betterbanking.models.Transaction> findAllByAccountNumber(Long accountNumber);
+    List<Transaction> findAllByAccountNumber(final Integer accountNumber);
 }
