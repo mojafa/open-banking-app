7c59b6d08c08c603072cbd56bb22412403707c09
MoJafa <jmohsa@gmail.com>
2023-04-10 19:40:13 +0200

    controller test

--- src/test/java/com/mojafa/betterbanking/controllers/TransactionControllerTest.java
+++ src/test/java/com/mojafa/betterbanking/controllers/TransactionControllerTest.java
@@ -1,27 +1,34 @@
 package com.mojafa.betterbanking.controllers;
-
-import org.junit.Test;
+import mojafa.betterbanking.BetterBankingApplication;
+import mojafa.betterbanking.models.Transaction;
+import mojafa.betterbanking.services.TransactionService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(classes = BetterBankingApplication.class)
 public class TransactionControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @MockBean private TransactionService transactionService;
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void testTransactions() throws Exception {
+        doReturn(List.of(new Transaction())).when(transactionService).findAllByAccountNumber(anyInt());
+
         mockMvc.perform(
-                get("/api/v1/transactions/1"))
-                .andDo(print()).andExpect(status().isOk());
+                        get("/api/v1/transactions/12345678"))
+                .andExpect(status().isOk());
     }
 
+    @Autowired private MockMvc mockMvc;
 }
