f82499016f00ecbb5a8d187c37c643f1d6e6afcc
MoJafa <jmohsa@gmail.com>
2023-04-10 10:43:59 +0200

    modified adapter to return transactions mapped to my transaction model

--- src/main/java/mojafa/betterbanking/adapters/acme/OBTransactionAdapter.java
+++ src/main/java/mojafa/betterbanking/adapters/acme/OBTransactionAdapter.java
@@ -1,22 +1,25 @@
 package mojafa.betterbanking.adapters.acme;
 
 import com.acme.banking.model.OBTransaction6;
-import com.mojafa.betterbanking.models.Transaction;
+import mojafa.betterbanking.models.Transaction;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.Date;
 
 public class OBTransactionAdapter {
-    public com.mojafa.betterbanking.models.Transaction adapt(final OBTransaction6 transaction6){
+
+    public Transaction adapt(final OBTransaction6 transaction6) {
         return transactionFunction.apply(transaction6);
     }
-    private final Function<OBTransaction6, com.mojafa.betterbanking.models.Transaction> transactionFunction = obTransaction6 ->{
+
+    private final Function<OBTransaction6, Transaction> transactionFunction = obTransaction6 -> {
         var t = new Transaction();
         t.setAccountNumber(Integer.valueOf(obTransaction6.getAccountId()));
+
         var amount = tryAndApply(obTransaction6, o -> Double.valueOf(o.getAmount().getAmount()));
         var fx = tryAndApply(obTransaction6, o -> o.getCurrencyExchange().getExchangeRate().doubleValue());
-        t.setAmount((int) (amount.orElse(0.0) * fx.orElse(1.0)));
+        t.setAmount((double) (amount.orElse(0.0) * fx.orElse(1.0)));
 
         tryAndApply(obTransaction6, o -> obTransaction6.getCurrencyExchange().getUnitCurrency())
                 .ifPresent(t::setCurrency);
@@ -24,10 +27,11 @@ public class OBTransactionAdapter {
                 .ifPresent(t::setType);
         tryAndApply(obTransaction6, o -> obTransaction6.getMerchantDetails().getMerchantName())
                 .ifPresent(t::setMerchantName);
-        tryAndApply(obTransaction6, o -> new Date(obTransaction6.getValueDateTime().toDate().toEpochDay()))
+        tryAndApply(obTransaction6, o -> new Date(obTransaction6.getValueDateTime().toInstant().toEpochMilli()))
                 .ifPresent(t::setDate);
         return t;
     };
+
     private <T> Optional<T> tryAndApply(final OBTransaction6 transaction6, Function<OBTransaction6, T> func) {
         try {
             return Optional.of(func.apply(transaction6));
---
+++ src/test/java/com/mojafa/betterbanking/adapters/acme/OBTransactionAdapterTest.java
@@ -0,0 +1,2 @@
+package com.mojafa.betterbanking.adapters.acme;public class OBTransactionAdapterTest {
+}
