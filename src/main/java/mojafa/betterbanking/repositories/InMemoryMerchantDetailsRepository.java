5ae59310ecf0f350f4333cbb95c50481977155dd
MoJafa <jmohsa@gmail.com>
2023-04-10 11:32:30 +0200

    implemented merchantdeatilsrepo to hold logos

--- src/main/java/mojafa/betterbanking/repositories/InMemoryMerchantDetailsRepository.java
+++ src/main/java/mojafa/betterbanking/repositories/InMemoryMerchantDetailsRepository.java
@@ -1,4 +1,17 @@
 package mojafa.betterbanking.repositories;
 
-public class InMemoryMerchantDetailsRepository {
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class InMemoryMerchantDetailsRepository implements MerchantDetailsRepository {
+
+    @Override
+    public Optional<String> findMerchantLogo(String merchantName) {
+        if (merchantName == null) {
+            return Optional.empty();
+        }
+        return Optional.of(merchantName + ".png");
+    }
 }
