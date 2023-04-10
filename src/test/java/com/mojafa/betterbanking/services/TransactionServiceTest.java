22d0bea4ce9c8f8d2a67c0ec5a5370cbf43a29b3
MoJafa <jmohsa@gmail.com>
2023-04-10 11:42:31 +0200

    wrote transaction service test

--- src/test/java/com/mojafa/betterbanking/services/TransactionServiceTest.java
+++ src/test/java/com/mojafa/betterbanking/services/TransactionServiceTest.java
@@ -1,18 +1,35 @@
 package com.mojafa.betterbanking.services;
 
+import com.mojafa.betterbanking.BetterBankingApplication;
+import mojafa.betterbanking.models.Transaction;
+import mojafa.betterbanking.repositories.MerchantDetailsRepository;
+import mojafa.betterbanking.repositories.TransactionApiClient;
+import mojafa.betterbanking.services.TransactionService;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class TransactionServiceTest {
-    @Autowired
-    private com.mojafa.betterbanking.services.TransactionService transactionService;
+@SpringBootTest(classes = {BetterBankingApplication.class})
+public class TransactionServiceTest {
+    @Mock private TransactionApiClient transactionApiClient;
+    @Mock private MerchantDetailsRepository merchantDetailsRepository;
+    @InjectMocks private TransactionService transactionService;
+
+    @DisplayName("test TransactionService with mocj TransactionApiClient")
     @Test
     public void testTransactionCount() {
+        when(transactionApiClient.findAllByAccountNumber(any()))
+                .thenReturn(List.of(new Transaction()));
         assertEquals(1, transactionService.findAllByAccountNumber(1234567).size());
     }
 }
