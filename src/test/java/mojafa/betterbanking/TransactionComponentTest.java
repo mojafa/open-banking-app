8ebd0e00a3f701f89196e97a4817ff4cf3ec704e
MoJafa <jmohsa@gmail.com>
2023-04-11 07:56:09 +0200

    added tests

--- src/test/java/mojafa/betterbanking/TransactionComponentTest.java
+++ src/test/java/mojafa/betterbanking/TransactionComponentTest.java
@@ -1,14 +1,33 @@
 package mojafa.betterbanking;
 
-import mojafa.betterbanking.BetterBankingApplication;
+import com.acme.banking.model.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.module.mockmvc.config.RestAssuredMockMvcConfig;
 import mojafa.betterbanking.controllers.TransactionController;
-import org.hamcrest.Matchers;
+import mojafa.betterbanking.models.Transaction;
+import mojafa.betterbanking.repositories.InMemoryMerchantDetailsRepository;
+import mojafa.betterbanking.repositories.RESTTransactionsAPIClient;
+import mojafa.betterbanking.repositories.TransactionApiClient;
+import mojafa.betterbanking.repositories.TransactionRepository;
+import mojafa.betterbanking.services.TransactionService;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.config;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.config.MockMvcConfig.mockMvcConfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = {BetterBankingApplication.class})
 public class TransactionComponentTest {
@@ -16,15 +35,71 @@ public class TransactionComponentTest {
     @LocalServerPort
     private int port;
 
-    @Autowired
-    private TransactionController transactionController;
+    @MockBean
+    private TransactionRepository transactionRepository;
 
+
+    private MockWebServer server = new MockWebServer();
+    private TransactionApiClient apiClient = new RESTTransactionsAPIClient(WebClient.create(server.url("/").toString()));
+    private TransactionService transactionService = new TransactionService(apiClient, new InMemoryMerchantDetailsRepository(), transactionRepository);
     @Test
-    public void testApplicationEndToEnd() {
-        given().standaloneSetup(transactionController)
-                .when()
-                .get(String.format("http://localhost:%s/api/v1/transactions/1234567", port))
-                .then()
-                .statusCode(Matchers.is(200));
+    @WithMockUser(username = "mo", password = "password", roles = "1234567")
+    public void testApplicationEndToEnd() throws Exception {
+        var json = new ObjectMapper().writeValueAsString(transaction());
+        server.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(json));
+
+        var t = Arrays.stream(
+                        given()
+                                .config(noSecurity())
+                                .standaloneSetup(new TransactionController(transactionService))
+                                .when()
+                                .get(String.format("http://localhost:%s/api/v1/transactions/1234567", port))
+                                .then()
+                                .extract()
+                                .body()
+                                .as(Transaction[].class))
+                .findAny()
+                .orElseThrow();
+
+        assertEquals(100.0d, t.getAmount());
+    }
+
+    private Object transaction() {
+        var t = new OBReadTransaction6();
+        t.setData(new OBReadDataTransaction6());
+        t.getData().addTransactionItem(transactions());
+        return t;
+    }
+
+    private OBTransaction6 transactions() {
+        var t = new OBTransaction6();
+        t.setAccountId("1234567");
+        t.setCreditDebitIndicator(OBCreditDebitCode1.DEBIT);
+        t.setAmount(amount());
+        t.setMerchantDetails(merchantDetails());
+        return t;
+    }
+
+    private OBActiveOrHistoricCurrencyAndAmount9 amount() {
+        var amount = new OBActiveOrHistoricCurrencyAndAmount9();
+        amount.setAmount("100.00");
+        amount.setCurrency("USD");
+        return amount;
+    }
+
+    private OBMerchantDetails1 merchantDetails() {
+        var m = new OBMerchantDetails1();
+        m.setMerchantName("acme");
+        m.setMerchantCategoryCode("25");
+        return m;
+    }
+
+    private RestAssuredMockMvcConfig noSecurity() {
+        return config().mockMvcConfig(mockMvcConfig()
+                .dontAutomaticallyApplySpringSecurityMockMvcConfigurer());
     }
 }
--- src/test/java/mojafa/betterbanking/controllers/TransactionControllerTest.java
+++ src/test/java/mojafa/betterbanking/controllers/TransactionControllerTest.java
@@ -4,31 +4,78 @@ import mojafa.betterbanking.models.Transaction;
 import mojafa.betterbanking.services.TransactionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @AutoConfigureMockMvc
 @SpringBootTest(classes = BetterBankingApplication.class)
 public class TransactionControllerTest {
 
-    @MockBean private TransactionService transactionService;
+    private static final String CLIENT_ID = "open-banking";
+    private static final String CLIENT_SECRET = "sVJcgeBD18w5ytF9VUcQMUzJSAN6qg3O";
+
+    @MockBean
+    private TransactionService transactionService;
 
     @Test
-    public void testTransactions() throws Exception {
+    public void testUnauthenticatedTransactions() throws Exception {
         doReturn(List.of(new Transaction())).when(transactionService).findAllByAccountNumber(anyInt());
 
         mockMvc.perform(
-                        get("/api/v1/transactions/12345678"))
-                .andExpect(status().isOk());
+                        get("/api/v1/transactions/123456"))
+                .andExpect(status().is3xxRedirection());
     }
 
-    @Autowired private MockMvc mockMvc;
+    @Test
+    public void testAuthenticatedTransactions() throws Exception {
+        String accessToken = obtainAccessToken("mo", "password");
+
+        mockMvc.perform(get("/api/v1/transactions/123456")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .accept("application/json;charset=UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().contentType("application/json;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    private String obtainAccessToken(String username, String password) throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", CLIENT_ID);
+        params.add("client_secret", CLIENT_SECRET);
+        params.add("username", username);
+        params.add("password", password);
+
+        var webClient = WebClient.create("http://localhost:9001");
+        var resultString = webClient.post()
+                .uri("/auth/realms/master/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(params))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("access_token").toString();
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
 }
