9c9bd72bb90279d5f041f63d2b1fcf5d5933d55b
MoJafa <jmohsa@gmail.com>
2023-04-11 07:43:39 +0200

    updated controller to use dto and post filter

--- src/main/java/mojafa/betterbanking/controllers/TransactionController.java
+++ src/main/java/mojafa/betterbanking/controllers/TransactionController.java
@@ -1,13 +1,19 @@
 package mojafa.betterbanking.controllers;
 
+import mojafa.betterbanking.dto.TransactionDTO;
 import mojafa.betterbanking.models.Transaction;
 import mojafa.betterbanking.services.TransactionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
@@ -18,9 +24,19 @@ public class TransactionController {
     }
 
     @GetMapping("/{accountNumber}")
-    public List<Transaction> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber)
-            throws Exception {
-        return transactionService.findAllByAccountNumber(accountNumber);
+    public ResponseEntity<List<TransactionDTO>> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber) throws Exception {
+        var list = transactionService.findAllByAccountNumber(accountNumber)
+                .stream().map(this::map)
+                .collect(toList());
+
+        if (list.isEmpty()){
+            return  notFound().build();
+        }
+        return ok(list);
+    }
+
+    private TransactionDTO map(final Transaction tr) {
+        return TransactionDTO.apply(tr);
     }
 
     private final TransactionService transactionService;
