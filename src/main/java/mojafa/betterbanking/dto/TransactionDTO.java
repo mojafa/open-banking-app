9ce6451a2fe756728f9c285cf2c7ea1baf33ad07
MoJafa <jmohsa@gmail.com>
2023-04-11 07:44:34 +0200

    dto update

--- src/main/java/mojafa/betterbanking/dto/TransactionDTO.java
+++ src/main/java/mojafa/betterbanking/dto/TransactionDTO.java
@@ -1,2 +1,34 @@
-package mojafa.betterbanking.dto;public class TransactionDTO {
+package mojafa.betterbanking.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import mojafa.betterbanking.models.Transaction;
+
+import java.util.Date;
+
+
+@Data
+@Builder
+public class TransactionDTO {
+    private Long id;
+    private String type;
+    private Date date;
+    private Integer accountNumber;
+    private String currency;
+    private Double amount;
+    private String merchantName;
+    private String merchantLogo;
+
+    public static TransactionDTO apply(final Transaction tr) {
+        var t = new TransactionDTOBuilder()
+                .id(tr.getId())
+                .type(tr.getType())
+                .accountNumber(tr.getAccountNumber())
+                .currency(tr.getCurrency())
+                .amount(tr.getAmount())
+                .merchantName(tr.getMerchantName())
+                .merchantLogo(tr.getMerchantLogo())
+                .build();
+        return t;
+    }
 }
